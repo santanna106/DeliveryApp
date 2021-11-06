8259f624e7df473a42c152c6a9d36ad97336ca4b
Gabriel Andrade <gabriel.santana@gmail.com>
2021-11-06 07:30:34 -0300

    Inclusão do google Maps

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
+import MapView from 'react-native-maps';
 import { Title } from 'react-native-paper';
 
+const LATITUDE_DELTA:number = 0.09;
+const LOGITUDE_DELTA:number = 0.04; 
+
 export const Home:React.FC = () => {
+
     return (
-        <SafeAreaView>
-            <Title>Home Screen</Title>
+        <SafeAreaView style={{flex:1}}>
+            <MapView
+                style={{flex:1}}
+                initialRegion={{
+                    latitude:-3.7222,
+                    longitude:-38.515,
+                    latitudeDelta:LATITUDE_DELTA,
+                    longitudeDelta:LOGITUDE_DELTA
+                }} 
+            >
+            </MapView>
         </SafeAreaView>
     )
 }
